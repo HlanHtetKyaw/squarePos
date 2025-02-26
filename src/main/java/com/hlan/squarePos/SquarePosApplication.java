e24cc8dca102e7c653f275ab09dbdc9ed8fefefb
square <hlan1559@gmail.com>
2025-02-26 18:26:50 +0630

    [ADD] ReadMe

---
+++ src/main/java/com/hlan/squarePos/SquarePosApplication.java
@@ -0,0 +1,13 @@
+package com.hlan.squarePos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarePosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquarePosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlan/squarePos/SquarePosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlan.squarePos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarePosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
